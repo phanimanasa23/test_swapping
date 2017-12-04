cda0926709c8c557c7e43130951386d82f1978e2
Manasa <phanimanasa23@gmail.com>
2017-12-04 13:12:08 +0530

    checking conflict

--- testFile.js
+++ testFile.js
@@ -6,4 +6,4 @@ testing .....my lines of testing.......
 in forked repository!!!!!
 ***************************new line added
 and here goes the commit sequence
-and this is second for commit sequence
+and that is second for commit sequence
